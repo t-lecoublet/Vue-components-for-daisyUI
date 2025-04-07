b861ac6a9760bf558f6d17a802af554553a870b4
Theo LECOUBLET <theo.lecoublet@uca.fr>
2025-04-07 16:20:13 +0200

    Add Navbar in Navigation

--- src/components/Navigation/Navbar.stories.ts
+++ src/components/Navigation/Navbar.stories.ts
@@ -4,119 +4,125 @@ import ButtonLink from "@/components/Navigation/ButtonLink.vue";
 import Button from "@/components/Actions/Button.vue";
 import EllipsisIcon from "@/components/Icons/EllipsisIcon.vue";
 import MenuIcon from "@/components/Icons/MenuIcon.vue";
+import Avatar from '../DataDisplay/Avatar.vue'
+import Dropdown from "@/components/Actions/Dropdown.vue";
+import Menu from "@/components/Navigation/Menu.vue";
 
 const meta: Meta<typeof Navbar> = {
   title: "Components/Navigation/Navbar",
   component: Navbar,
-  argTypes: {},
 };
 
 export default meta;
+type Story = StoryObj<typeof meta>;
 
-type Story = StoryObj<typeof Navbar>;
-
-const DefaultTplStr = `
-<Navbar>
-  <ButtonLink href="/" :ghost="true" >daisyUI</ButtonLink>
-</Navbar>`;
-const WithTitleAndIconTplStr = `
-<Navbar>
-  <div class="flex-1">
-    <ButtonLink href="/" :ghost="true" >daisyUI</ButtonLink>
-  </div>
-  <div class="flex-none">
-    <Button :ghost="true" :square="true" >
-      <EllipsisIcon />
-    </Button>
-  </div>
-</Navbar>`;
-const WithIconAtStartAndEndTplStr = `
-<Navbar>
-  <div class="flex-none">
-    <Button :ghost="true" :square="true" >
-      <MenuIcon />
-    </Button>
-  </div>
-  <div class="flex-1">
-    <ButtonLink href="/" :ghost="true" >daisyUI</ButtonLink>
-  </div>
-  <div class="flex-none">
-    <Button :ghost="true" :square="true" >
-      <EllipsisIcon />
-    </Button>
-  </div>
-</Navbar>`;
-const NavbarWithMenuAndSubmenuTplStr = `
-<Navbar>
-  <div class="flex-1">
-    <ButtonLink class="text-xl" :ghost="true">daisyUI</ButtonLink>
-  </div>
-  <div class="flex-none">
-    <ul class="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul class="bg-base-100 rounded-t-none p-2">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
-          </ul>
-        </details>
-      </li>
-    </ul>
-  </div>
-</Navbar>`;
-
-// NAVBAR WITH TITLE ONLY
-
-const NavbarWithTitleOnlyTemplate: Story = {
-  render: (args: any) => ({
+export const Default: Story = {
+  render: () => ({
     components: { Navbar, ButtonLink },
-    setup() {
-      return { args };
-    },
-    template: DefaultTplStr,
-  }),
-};
-export const NavbarWithTitleOnly = { ...NavbarWithTitleOnlyTemplate };
+    template: `
+      <Navbar>
+        <template #center>
+          <ButtonLink href="/" :ghost="true">daisyUI</ButtonLink>
+        </template>
+      </Navbar>
+    `
+  })
+}
 
-// NAVBAR WITH TITLE AND ICON
-
-const NavbarWithTitleAndIconTemplate: Story = {
-  render: (args: any) => ({
+export const WithTitleAndIcon: Story = {
+  render: () => ({
     components: { Navbar, ButtonLink, Button, EllipsisIcon },
-    setup() {
-      return { args };
-    },
-    template: WithTitleAndIconTplStr,
-  }),
-};
-export const NavbarWithTitleAndIcon = { ...NavbarWithTitleAndIconTemplate };
+    template: `
+      <Navbar>
+        <template #start>
+          <ButtonLink href="/" :ghost="true">daisyUI</ButtonLink>
+        </template>
+        <template #end>
+          <Button :ghost="true" :square="true">
+            <EllipsisIcon />
+          </Button>
+        </template>
+      </Navbar>
+    `
+  })
+}
 
-// NAVBAR WITH ICON AT START AND END
-
-const NavbarWithIconAtStartAndEndTemplate: Story = {
-  render: (args: any) => ({
+export const WithIconAtStartAndEnd: Story = {
+  render: () => ({
     components: { Navbar, ButtonLink, Button, EllipsisIcon, MenuIcon },
-    setup() {
-      return { args };
-    },
-    template: WithIconAtStartAndEndTplStr,
-  }),
-};
-export const NavbarWithIconAtStartAndEnd = {
-  ...NavbarWithIconAtStartAndEndTemplate,
-};
+    template: `
+      <Navbar>
+        <template #start>
+          <Button :ghost="true" :square="true">
+            <MenuIcon />
+          </Button>
+        </template>
+        <template #center>
+          <ButtonLink href="/" :ghost="true">daisyUI</ButtonLink>
+        </template>
+        <template #end>
+          <Button :ghost="true" :square="true">
+            <EllipsisIcon />
+          </Button>
+        </template>
+      </Navbar>
+    `
+  })
+}
 
-// NAVBAR WITH MENU AND SUBMENU
-
-const NavbarWithMenuAndSubmenuTemplate: Story = {
-  render: (args: any) => ({
+export const WithMenuAndSubmenu: Story = {
+  render: () => ({
     components: { Navbar, ButtonLink },
-    setup() {
-      return { args };
-    },
-    template: NavbarWithMenuAndSubmenuTplStr,
-  }),
-};
-export const NavbarWithMenuAndSubmenu = { ...NavbarWithMenuAndSubmenuTemplate };
+    template: `
+      <Navbar>
+        <template #start>
+          <ButtonLink class="text-xl" :ghost="true">daisyUI</ButtonLink>
+        </template>
+        <template #end>
+          <ul class="menu menu-horizontal px-1">
+            <li><a>Link</a></li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul class="bg-base-100 rounded-t-none p-2">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </template>
+      </Navbar>
+    `
+  })
+}
+
+export const WithSearchAndAvatar: Story = {
+  render: () => ({
+    components: { Navbar, Button, Avatar, Dropdown, Menu },
+    template: `
+      <Navbar>
+        <template #start>
+          <Button variant="ghost" class="text-xl">daisyUI</Button>
+        </template>
+        <template #end>
+          <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+            <Dropdown placement="end">
+              <template #trigger>
+                <Button class="m-1 overflow-hidden" :circle="true">
+                  <Avatar>
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" alt="" />
+                  </Avatar>
+                </Button>
+              </template>
+              <Menu class="w-56">
+                <li><a>Item 1</a></li>
+                <li><a>Item 2</a></li>
+                <li><a>Item 3</a></li>
+              </Menu>
+            </Dropdown>
+        </template>
+      </Navbar>
+    `
+  })
+}
